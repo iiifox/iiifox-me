36bc310c464c4c718f102b8b306c360f19e86f2d
iiifox <86429225+iiifox@users.noreply.github.com>
2025-12-05 00:33:18 +0800

    Create fanpiao.js

---
+++ assets/giraffe/fanpiao.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         fanpiao
+// @namespace    https://iiifox.me/
+// @version      3.3.3
+// @description  fanpiao
+// @author       iiifox
+// @match        *://pay.qq.com/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// @updateURL    https://iiifox.me/assets/giraffe/fanpiao.js
+// @downloadURL  https://iiifox.me/assets/giraffe/fanpiao.js
+// @connect      081w5a8cim.top
+// @connect      8w0m6rjg3l.top
+// ==/UserScript==
+
+
+(function () {
+    'use strict';
+
+    
+
+})();
